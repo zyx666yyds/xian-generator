6149b6c09a8fdccdaeb44e9d51ca54ed4fe3461f
迷寂 <2408893294@qq.com>
2024-01-22 15:19:40 +0800

    跑通流程，解决之前的所有bug

--- xian-generator-maker/src/main/java/com/zyx/maker/generator/ScriptGenerator.java
+++ xian-generator-maker/src/main/java/com/zyx/maker/generator/ScriptGenerator.java
@@ -1,14 +1,15 @@
 package com.zyx.maker.generator;
 
+
 import cn.hutool.core.io.FileUtil;
 
-import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.attribute.PosixFilePermission;
 import java.nio.file.attribute.PosixFilePermissions;
 import java.util.Set;
+
 
 /**
  * @author zyx
@@ -29,7 +30,7 @@ public class ScriptGenerator {
         try {
             Set<PosixFilePermission> permissions = PosixFilePermissions.fromString("rwxrwxrwx");
             Files.setPosixFilePermissions(Paths.get(outputPath), permissions);
-        } catch (IOException ignored) {
+        } catch (Exception ignored) {
 
         }
 
